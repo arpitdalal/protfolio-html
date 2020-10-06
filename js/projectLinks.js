cbad10b14a4ac82e41e354c0a20c68739b997901
Arpit <arpitdalal21@gmail.com>
2020-10-05 21:03:04 -0300

    added ctas on projects for desktop too

--- js/projectLinks.js
+++ js/projectLinks.js
@@ -3,3 +3,9 @@ jQuery(document).on('click', '.project-links', function(e) {
   let projectLinkName = jQuery(this).attr('name');
   return $load.load(`/projects/${projectLinkName} #${projectLinkName}Div`);
 });
+
+jQuery(document).on('click', '.in-project-link', function(e) {
+  e.preventDefault();
+  let inProjectLinkName = jQuery(this).attr('name');
+  return $load.load(`/${inProjectLinkName} #${inProjectLinkName}Div`);
+});
